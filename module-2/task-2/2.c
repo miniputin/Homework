4ff56d4b927bc53ed6395ae143af19c19b5b6fcf
miniputin <masha199.7@inbox.ru>
2016-04-29 00:32:38 +0400

    Create 2.c

---
+++ module-2/task-2/2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+printf(" | NUL| SOH| STX| ETX| EOT| ENQ| ACK| BEL| BS | HT | LF | VT | FF | CR | SO | SI |\n");
+printf(" | DLE| DC1| DC2| DC3| DC4| NAK| SYN| ETB| CAN| EM | SUB| ESC| FS | GS | RS | US | DEL|\n");
+printf(" |  ! | ? | \" | @ |  # |  $ | %% |  & |  ' | ` |  ( |  ) |  * |  + |  , |  - |  . |  / |\n"); 
+printf(" |  0 |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  : |  ; |  < |  > |  = |\n"); 
+printf(" |  A |  B |  C |  D |  E |  F |  G |  H |  I |  J |  K |  L |  M |  N |  O |\n");
+printf(" |  P |  Q |  R |  S |  T |  U |  V |  W |  X |  Y |  Z |  [ | \\ |  ] |  ^ |  _ |\n");
+printf(" |  a |  b |  c |  d |  e |  f |  g |  h |  i |  j |  k |  l |  m |  n |  o |\n");
+printf(" |  p |  q |  r |  s |  t |  u |  v |  w |  x |  y |  z |  { |  | |  } |  ~ |\n");
+return 0;
+}
